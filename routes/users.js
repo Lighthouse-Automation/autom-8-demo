c2b2be27987bb17dafb1c86e63254fc7c6519afd
DeanS <dean@sellers.id.au>
2015-05-27 20:13:20 +1000

    Auth post redirects unless xhr request, otherwise redirect back.

--- routes/users.js
+++ routes/users.js
@@ -9,18 +9,25 @@ router.get('/', function (req, res, next) {
 });
 
 /* POST login, auth using passport and then generate a jwt and return it,
-  otherwise 401 */
+  otherwise redirect back, or 401 if xhr */
 router.post('/', function (req, res, next) {
   passport.authenticate('local-login', function (err, usrObj, info) {
     //Auth failed for some reason
     if (err) {
       return next(err);
     }
-    //The usrObj is invalid
-    if (!usrObj) {
-      res.json(401, {error: 'Authentication error'});
+    if (!usrObj) { //The usrObj is invalid, which means auth has failed
+      if (req.xhr) { //An ajax auth request gets a 401
+        res.status(401).json({reason: 'Authentication error'});
+      } else {  //Any other request gets redirected
+        //The main app error handler redirects back to the original request
+        var newErr = new Error('Authentication error');
+        newErr.status = 302;
+        newErr.name = 'Unauthorized'; //I know the status is 302, but this is the real cause
+        return next(newErr);
+      }
     }
-    //We have a valid usrObj, generate a jwt and return it as json
+    //We have a valid usrObj, Auth sucessful. Generate a jwt and return it as json
     var token = jwt.encode({user: usrObj.user}, 'the_secret');
     res.json({ token: token });
   })(req, res, next);
